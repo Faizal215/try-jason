6f30de48e095e7ffe05df9403a7a12018e08733a
Faizal215 <127084108+Faizal215@users.noreply.github.com>
2023-04-05 20:23:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import json
 import pandas as pd
 from flask import Flask, request, render_template
 import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
 
 app = Flask(__name__, template_folder='template')
 
